460a575dc398067440783bf91622650c986f9fcb
Silhouette <54932288+Silhouette-sophist@users.noreply.github.com>
2025-06-15 10:16:50 +0800

    优化

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23.0
 toolchain go1.23.9
 
 require golang.org/x/mod v0.24.0
+
+require github.com/cloudwego/hertz v0.10.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"ast-callgraph/service"
+	"context"
+	"time"
 
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+)
+
+func main() {
+	ctx := context.Background()
+	start := time.Now()
+	defer func() {
+		hlog.CtxInfof(ctx, "exec cost %.2f", time.Since(start).Seconds())
+	}()
+	astTransverseInfo, err := service.TransverseDirectory(ctx, &service.AstTransverseParam{
+		Directory: "/Users/silhouette/work-practice/ast-callgraph",
+	})
+	if err != nil {
+		hlog.CtxInfof(ctx, "TransverseDirectory err: %v", err)
+		return
+	}
+	hlog.CtxInfof(ctx, "TransverseDirectory success %v", astTransverseInfo)
 }
